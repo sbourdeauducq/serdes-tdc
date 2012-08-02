439736d5dc072f16a94e308aa0476514a7cd677f
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-08-02 14:17:27 +0200

    Update software to new interface

--- demo/software/demo/stdc.c
+++ demo/software/demo/stdc.c
@@ -4,11 +4,24 @@
 
 #include "stdc.h"
 
+static void report_overflows(void)
+{
+	if(CSR_TDC0_OVERFLOW) {
+		printf("FIFO0 overflow\n");
+		CSR_TDC0_OVERFLOW = 1;
+	}
+	if(CSR_TDC1_OVERFLOW) {
+		printf("FIFO1 overflow\n");
+		CSR_TDC1_OVERFLOW = 1;
+	}
+}
+
 void tdc(void)
 {
 	while(1) {
 		while(!CSR_TDC0_EVENT && !CSR_TDC1_EVENT) {
 			if(readchar_nonblock()) return;
+			report_overflows();
 		}
 		if(CSR_TDC0_EVENT) {
 			printf("0,%u,%u\n", CSR_TDC0_POLARITY, CSR_TDC0_TIMESTAMP);
@@ -23,13 +36,14 @@ void tdc(void)
 
 void diff(void)
 {
-	CSR_TDC0_EVENT = 1;
-	CSR_TDC1_EVENT = 1;
-	CSR_TDC0_EVENT = 1;
+	CSR_TDC0_FIFOCLEAR = 1;
+	CSR_TDC1_FIFOCLEAR = 1;
+	CSR_TDC0_FIFOCLEAR = 1;
 	
 	while(1) {
 		while(!(CSR_TDC0_EVENT && CSR_TDC1_EVENT)) {
 			if(readchar_nonblock()) return;
+			report_overflows();
 		}
 		if(CSR_TDC0_POLARITY != CSR_TDC1_POLARITY)
 			printf("Inconsistent polarities!\n");
